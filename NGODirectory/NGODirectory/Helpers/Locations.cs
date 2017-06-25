bdd8aa4292c99ff2c4922f87b7f3a9506d3329b2
Axel Gorris <axel.gd@outlook.com>
2017-06-25 20:13:25 +0200

    Cleanup

--- NGODirectory/NGODirectory/Helpers/Locations.cs
+++ NGODirectory/NGODirectory/Helpers/Locations.cs
@@ -8,18 +8,7 @@ namespace NGODirectory.Helpers
 {
     public static class Locations
     {
-//#if DEBUG
-        //public static readonly string AppServiceUrl = "http://localhost:50161/";
-        //public static readonly string AlternateLoginHost = "https://ngodirectory.azurewebsites.net";
-//#else
         public static readonly string AppServiceUrl = "https://ngodirectory.azurewebsites.net";
         public static readonly string AlternateLoginHost = null;
-//#endif
-
-        public static readonly string AadClientId = "a9c26aea-778e-450c-b2d0-f32ecd78cbfb";
-
-        public static readonly string AadRedirectUri = "https://ngodirectory.azurewebsites.net/.auth/login/done";
-
-        public static readonly string AadAuthority = "https://login.windows.net/axelgdoutlook.onmicrosoft.com";
     }
 }
